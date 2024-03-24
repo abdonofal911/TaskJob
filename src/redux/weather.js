b6b16caa9d687e76985fbffa577eef4f08a2a08b
abdonofal911 <abdonofa1712@gmail.com>
2024-03-24 11:06:31 +0200

    adding some touches

--- src/redux/weather.js
+++ src/redux/weather.js
@@ -1,21 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {API, apiKey} from '../apis';
 
-export const getWeather = createAsyncThunk(
-  'weather/getWeather',
-  async (city, {rejectWithValue}) => {
-    try {
-      const response = await API.get(
-        `/current.json?key=${apiKey}&q=${city}&aqi=no`,
-      );
-      const weatherData = response.data;
-      return weatherData;
-    } catch (error) {
-      console.error('Error fetching weather:', error);
-      return rejectWithValue(error);
-    }
-  },
-);
 export const getWeatherForecast = createAsyncThunk(
   'weather/getWeatherForecast',
   async (city, {rejectWithValue}) => {
@@ -25,19 +10,6 @@ export const getWeatherForecast = createAsyncThunk(
       );
       const weatherData = response.data;
       return weatherData;
-    } catch (error) {
-      console.error('Error fetching weather:', error);
-      return rejectWithValue(error);
-    }
-  },
-);
-export const getLocations = createAsyncThunk(
-  'weather/getLocations',
-  async (city, {rejectWithValue}) => {
-    try {
-      const response = await API.get(`/search.json?key=${apiKey}&q=${city}`);
-      const locationsData = response.data;
-      return locationsData;
     } catch (error) {
       console.error('Error fetching weather:', error);
       return rejectWithValue(error);
@@ -63,23 +35,6 @@ const weatherSlice = createSlice({
     },
   },
   extraReducers: builder => {
-    builder.addCase(getWeather.fulfilled, (state, action) => {
-      state.weather = action.payload;
-      state.getWeatherLoader = false;
-      console.log('getWeather.fulfilled');
-    });
-    builder.addCase(getWeather.pending, (state, action) => {
-      state.getWeatherLoader = true;
-      console.log('getWeather.pending');
-    });
-    builder.addCase(getWeather.rejected, (state, action) => {
-      state.getWeatherLoader = false;
-      const errorResponse = action.payload.response;
-      console.log(
-        'getWeather.rejected ',
-        errorResponse.status + errorResponse.statusText,
-      );
-    });
     builder.addCase(getWeatherForecast.fulfilled, (state, action) => {
       state.weatherForecast = action.payload;
       state.getWeatherForecastLoader = false;
@@ -94,23 +49,6 @@ const weatherSlice = createSlice({
       const errorResponse = action.payload.response;
       console.log(
         'getWeatherForecast.rejected ',
-        errorResponse.status + errorResponse.statusText,
-      );
-    });
-    builder.addCase(getLocations.fulfilled, (state, action) => {
-      state.locationsSearch = action.payload;
-      state.getLocationsSearchLoader = false;
-      console.log('getLocations.fulfilled');
-    });
-    builder.addCase(getLocations.pending, (state, action) => {
-      state.getLocationsSearchLoader = true;
-      console.log('getLocations.pending');
-    });
-    builder.addCase(getLocations.rejected, (state, action) => {
-      state.getLocationsSearchLoader = false;
-      const errorResponse = action.payload.response;
-      console.log(
-        'getLocations.rejected ',
         errorResponse.status + errorResponse.statusText,
       );
     });
